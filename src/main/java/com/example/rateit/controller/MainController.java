51ff0284ceff354f4567151c0393422d27ff1cac
Chethan B <chethanborigin@gmail.com>
2022-02-06 10:17:18 +0530

    changed list method and modified git ignore

--- src/main/java/com/example/rateit/controller/MainController.java
+++ src/main/java/com/example/rateit/controller/MainController.java
@@ -224,6 +224,8 @@ public class MainController {
                 }
             }
             mav.addObject("tvList", tvList);
+        } else {
+            mav.setViewName("redirect:/");
         }
         return mav;
     }
